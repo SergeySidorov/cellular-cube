12d5fdbd37946b3d64e36ba8da9607895792c95e
ssidorov <ssserega@gmail.com>
2013-11-24 21:02:59 +0400

    prototype

---
+++ Assets/Scripts/CubeControl.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeControl : MonoBehaviour {
+	public float rotateSpeed=10f;
+
+	void Update () {
+		float deltaY= Time.deltaTime * Input.GetAxis("Horizontal") * rotateSpeed;
+		float deltaX= Time.deltaTime * Input.GetAxis("Vertical") * rotateSpeed;
+		transform.Rotate(deltaX,deltaY,0);
+	}
+}
---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControl : MonoBehaviour {
+	public Transform board;
+	public GameObject prefabCell;
+	public int numOfCells=100;
+
+	void Start () {
+		for(int i=0;i<numOfCells;i++) {
+			Vector3 position=new Vector3(Random.Range(-50,50),Random.Range(-50,50),-50);
+			GameObject cell=GameObject.Instantiate(prefabCell,position,Quaternion.identity) as GameObject;
+			cell.transform.parent=board;
+		}
+	}
+	
+}
